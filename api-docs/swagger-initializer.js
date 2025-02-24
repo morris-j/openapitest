41b9139152493e2b66270e6ef2f75bda93bbf553
James Morris <jmorris@kochava.com>
2025-02-24 11:51:43 -0800

    fix: try multiple entries

--- api-docs/swagger-initializer.js
+++ api-docs/swagger-initializer.js
@@ -3,7 +3,21 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    url: "../K4P_OpenAPI_Spec.yaml",
+    //url: "../K4P_OpenAPI_Spec.yaml",
+    urls: [
+      {
+        url: "../K4P_OpenAPI_Spec.yaml",
+        name: "Primary"
+      },
+      {
+        url: "../K4P_OpenAPI_Spec2.yaml",
+        name: "Secondary"
+      },
+      {
+        url: "../K4P_OpenAPI_Spec3.yaml",
+        name: "Another"
+      },
+    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
