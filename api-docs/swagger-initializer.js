261057bc8ab742002d6cf0f1c440631330add4af
James Morris <jmorris@kochava.com>
2025-02-28 12:57:20 -0800

    fix: update spec and branding

--- api-docs/swagger-initializer.js
+++ api-docs/swagger-initializer.js
@@ -1,33 +1,23 @@
 window.onload = function() {
   //<editor-fold desc="Changeable Configuration Block">
 
-//  const LogoPlugin = () => {
-//          return {
-//            components: {
-//                Logo: () => (
-//                    <img alt="My Logo" height="40" src="kochava_horizontal.png"/>
-//                  )
-//            }
-//          }
-//        }
-
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    //url: "../K4P_OpenAPI_Spec.yaml",
-    urls: [
-      {
-        url: "../K4P_OpenAPI_Spec.yaml",
-        name: "Primary"
-      },
-      {
-        url: "../K4P_OpenAPI_Spec2.yaml",
-        name: "Secondary"
-      },
-      {
-        url: "../K4P_OpenAPI_Spec3.yaml",
-        name: "Another"
-      },
-    ],
+    url: "../K4P_OpenAPI_Spec.yaml",
+//    urls: [
+//      {
+//        url: "../K4P_OpenAPI_Spec.yaml",
+//        name: "Primary"
+//      },
+//      {
+//        url: "../K4P_OpenAPI_Spec2.yaml",
+//        name: "Secondary"
+//      },
+//      {
+//        url: "../K4P_OpenAPI_Spec3.yaml",
+//        name: "Another"
+//      },
+//    ],
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
